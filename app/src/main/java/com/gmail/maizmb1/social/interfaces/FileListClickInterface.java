4f47af42b9a6dee8c1e5c5f3f8a1954bb72b7fbb
maiz bagwala <maizmb1@gmail.com>
2021-07-17 18:28:08 +0530

    Initial commit

---
+++ app/src/main/java/com/gmail/maizmb1/social/interfaces/FileListClickInterface.java
@@ -0,0 +1,7 @@
+package com.gmail.maizmb1.social.interfaces;
+
+import java.io.File;
+
+public interface FileListClickInterface {
+    void getPosition(int position, File file);
+}
---
+++ app/src/main/java/com/gmail/maizmb1/social/interfaces/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.gmail.maizmb1.social.interfaces;
+
+
+import com.gmail.maizmb1.social.model.story.TrayModel;
+
+public interface UserListInterface {
+    void userListClick(int position, TrayModel trayModel);
+}
